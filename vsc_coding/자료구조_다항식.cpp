def1455ad519c34059f8e07dd64d41bf5ae7a310
juyeon Lee <ljy436400@naver.com>
2022-01-20 09:43:39 +0900

    complite

--- vsc_coding/자료구조_다항식.cpp
+++ vsc_coding/자료구조_다항식.cpp
@@ -4,7 +4,7 @@ using namespace std;
 typedef class {
     public:
     int degree;
-    float coef[200];
+    float coef[200]={0};
 }poly;
 
 poly plus_both(poly one, poly two){
@@ -12,36 +12,52 @@ poly plus_both(poly one, poly two){
     int minus = one.degree - two.degree;
     if (minus<0) {
         final.degree = two.degree;
-        cout<<final.degree;
+        cout<<final.degree<<endl;
         for (int i=0; i<-(minus); i++)
         {
         final.coef[i] = two.coef[i];
-        cout<<final.coef[i];
+        cout<<final.coef[i]<<endl;
         }
         minus= -(minus);
+        int small = 0;
+        while ((one.coef[small]!=0)||(two.coef[minus]!=0))
+        {
+            final.coef[minus]=one.coef[small]+two.coef[minus];
+            minus++;
+            small++;
+        }
     }
     else if(minus==0){
         final.degree = two.degree;
-        cout<<final.degree;
+        cout<<final.degree<<endl;
+
+        int small = 0;
+        while ((one.coef[small]!=0)||(two.coef[small]!=0))
+        {
+            final.coef[small]=one.coef[small]+two.coef[small];
+            small++;
+        }
     }
     else {
         final.degree = one.degree;
-        cout<<final.degree;
+        cout<<final.degree<<endl;
         for (int i=0; i<minus; i++)
         {
         final.coef[i] = one.coef[i];
-        cout<<final.coef[i];
+        cout<<final.coef[i]<<endl;
+        }
+        int small = 0;
+        while ((one.coef[minus]!=0)||(two.coef[small]!=0))
+        {
+            final.coef[minus]=one.coef[minus]+two.coef[small];
+            minus++;
+            small++;
         }
     }
-    minus--;
-    while ((one.coef[minus]!=0)||(two.coef[minus]!=0))
+   
+    for (int i=0;i<final.degree;i++)
     {
-        final.coef[minus]=one.coef[minus]+two.coef[minus];
-        minus++;
-    }
-    for (int i=0;i<10;i++)
-    {
-        cout << final.coef[i];
+        cout << final.coef[i]<<',';
     }
     return final;
 
@@ -49,12 +65,9 @@ poly plus_both(poly one, poly two){
 
 int main()
 {
-    poly one= {5, {1,3,2,2,3}};
-    poly two= {7, {2,3,3,2,2,2,2}};
+    poly one= {7, {2,3,1,3,2,2,3}};
+    poly two= {4, {2,2,2,2}};
 
     poly final = plus_both(one, two);
-
-
-
-    
+   
 }
