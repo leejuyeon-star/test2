3381089e2beceb4a0dc5aad60de8784ffe52cc8f
juyeon Lee <ljy436400@naver.com>
2022-01-20 14:38:49 +0900

    complite

--- vsc_coding/자료구조_다항식2.cpp
+++ vsc_coding/자료구조_다항식2.cpp
@@ -4,40 +4,46 @@ using namespace std;
 
 typedef class{
     public:
-    int coef[20][3];
+    int coef[20][2];
 }poly;
 
 
 poly plus_both(poly one, poly two)
 {
     poly final;
-    int Apos=0, Bpos=0;
+    int Apos=0, Bpos=0, Cpos=0;
    while((one.coef[Apos][1]!=0)||(two.coef[Bpos][1]!=0))
     {
-        if (one.coef[Apos][0] > two.coef[Bpos][0])
+        if ((one.coef[Apos][0]) > (two.coef[Bpos][0]))
         {
-            final.coef[Apos][0]= one.coef[Apos][0];
-            final.coef[Apos][1]=one.coef[Apos][1];
+            final.coef[Cpos][0]= one.coef[Apos][0];
+            final.coef[Cpos][1]=one.coef[Apos][1];
             Apos++;
+            Cpos++;
+            cout<<"one big\n";
+
         }
 
-        else if (one.coef[Apos][0] < two.coef[Bpos][0])
+        else if ((one.coef[Apos][0]) < (two.coef[Bpos][0]))
         {
-            final.coef[Bpos][0]= one.coef[Bpos][0];
-            final.coef[Bpos][1]=one.coef[Bpos][1];
+            final.coef[Cpos][0]= two.coef[Bpos][0];
+            final.coef[Cpos][1]=two.coef[Bpos][1];
             Bpos++;
+            Cpos++;
+            cout<<"two big\n";
         }
 
-        else
+        else if(one.coef[Apos][0]==two.coef[Bpos][0])
         {
-            final.coef[Bpos][0]= one.coef[Bpos][0];
-            final.coef[Bpos][1]=one.coef[Bpos][1]+two.coef[Apos][1];
+            final.coef[Cpos][0]= one.coef[Apos][0];
+            final.coef[Cpos][1]=one.coef[Apos][1]+two.coef[Bpos][1];
             Apos++;
             Bpos++;
+            Cpos++;
         }
 
     }
-    for (int i=0;i<20;i++)
+    for (int i=0;i<Cpos;i++)
         {
             cout<<'['<<final.coef[i][0]<<','<<final.coef[i][1]<<"]";
         }
@@ -48,6 +54,5 @@ int main()
 {
     poly one= {10,1,1,2,0,3};
     poly two= {10,3,5,2,3,1,0,2};
-    
     poly final = plus_both(one, two);
 }
